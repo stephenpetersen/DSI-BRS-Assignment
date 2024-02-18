c3aef3054562d09dc2d2e5a6fa5bd90ef0e70e91
stephenpetersen <petersen.stephen@me.com>
2024-02-18 18:23:08 -0500

    Assignment.md to gitignore, edits to Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -7,10 +7,10 @@ import requests
 class Analysis():
 
     def __init__(self, analysis_config: str) -> None:
-        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
+        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml', 'configs/analysis.yml']
 
         # add the analysis config to the list of paths to load
-        paths = CONFIG_PATHS + [analysis_config]
+        paths = CONFIG_PATHS
 
         # initialize empty dictionary to hold the configuration
         config = {}
