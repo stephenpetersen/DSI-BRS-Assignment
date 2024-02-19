b8aa8d16aefca1e53568f995ac292e311bc9018d
stephenpetersen <petersen.stephen@me.com>
2024-02-19 09:34:23 -0500

    added docstring templates

--- Analysis.py
+++ Analysis.py
@@ -7,6 +7,34 @@ import requests
 class Analysis():
 
     def __init__(self, analysis_config: str) -> None:
+
+        ''' Load config into an Analysis object
+
+Load system-wide configuration from `configs/system_config.yml`, user configuration from
+`configs/user_config.yml`, and the specified analysis configuration file
+
+Parameters
+----------
+analysis_config : str
+    Path to the analysis/job-specific configuration file
+
+Returns
+-------
+analysis_obj : Analysis
+    Analysis object containing consolidated parameters from the configuration files
+
+Notes
+-----
+The configuration files should include parameters for:
+    * GitHub API token
+    * ntfy.sh topic
+    * Plot color
+    * Plot title
+    * Plot x and y axis titles
+    * Figure size
+    * Default save path
+
+'''
         CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
 
         # add the analysis config to the list of paths to load
@@ -24,15 +52,73 @@ class Analysis():
         self.config = config
 
     def load_data(self) -> None:
+        ''' Retrieve data from the GitHub API
+
+This function makes an HTTPS request to the GitHub API and retrieves your selected data. The data is
+stored in the Analysis object.
+
+Parameters
+----------
+None
+
+Returns
+-------
+None
+
+'''
+
         data = requests.get('/url/to/data').json()
         self.dataset = data
 
     def compute_analysis(self) -> Any:
+        '''Analyze previously-loaded data.
+
+This function runs an analytical measure of your choice (mean, median, linear regression, etc...)
+and returns the data in a format of your choice.
+
+Parameters
+----------
+None
+
+Returns
+-------
+analysis_output : Any
+
+'''
         return self.dataset.mean() 
 
     def plot_data(self, save_path: Optional[str] = None) -> plt.Figure:
+        ''' Analyze and plot data
+
+Generates a plot, display it to screen, and save it to the path in the parameter `save_path`, or 
+the path from the configuration file if not specified.
+
+Parameters
+----------
+save_path : str, optional
+    Save path for the generated figure
+
+Returns
+-------
+fig : matplotlib.Figure
+
+'''
         pass
 
     def notify_done(self, message: str) -> None:
+        ''' Notify the user that analysis is complete.
+
+Send a notification to the user through the ntfy.sh webpush service.
+
+Parameters
+----------
+message : str
+  Text of the notification to send
+
+Returns
+-------
+None
+
+'''
         pass
     
