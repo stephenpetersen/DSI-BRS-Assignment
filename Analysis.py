67e53901d4254401398f5f0af73506f6a897b656
stephenpetersen <petersen.stephen@me.com>
2024-02-19 09:18:14 -0500

    remove files

--- Analysis.py
+++ Analysis.py
@@ -5,7 +5,6 @@ import base64
 
 
 class Analysis():
-
     def __init__(self) -> None:
         CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml', 'configs/analysis_config.yml']
 
@@ -34,20 +33,34 @@ class Analysis():
         client_credentials = f"{client_id}:{client_secret}"
         client_credentials_b64 = base64.b64encode(client_credentials.encode()).decode()
 
-        # Define headers for authorization
-        headers = {'Authorization': f'Basic {client_credentials_b64}'}
-
-        # Define payload for token request
-        payload = {'grant_type': 'client_credentials'}
+        # Define headers and data
+        headers = {
+            'Authorization': f'Basic {client_credentials_b64}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {'grant_type': 'client_credentials'}
 
         # Make a POST request to get the access token
-        response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=payload)
+        response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
 
         # Check if request was successful
         if response.status_code == 200:
-            # Extract access token from response
+             # Extract access token from response
             access_token = response.json()['access_token']
-            print("Authentication successful! Access Token:", access_token)
+            print(f'Authentication successful! Here\'s the full response: {response.json()}')
+
             return access_token
         else:
             print("Authentication failed:", response.text)
+
+    def get_auth_header(self, access_token):
+        return {'Authorization': f'Bearer {access_token}'}
+    
+    def get_artist(self, access_token, artist_name):
+        url = 'https://api.spotify.com/v1/search'
+        headers = self.get_auth_header(access_token)
+        query = f'?q={artist_name}&type=artist&limit=1'
+
+        query_url = url + query
+        response = requests.get(query_url, headers=headers)
+        print(response.json())
---
+++ test_Analysis.py
@@ -0,0 +1,6 @@
+from Analysis import Analysis
+
+token = Analysis.get_token(Analysis)
+
+Analysis.get_artist(Analysis, , 'John Mayer')
+
